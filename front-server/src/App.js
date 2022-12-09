5207eedef299cf2aec956b36dfd457b832d919b2
sienna <20200803@sungshin.ac.kr>
2022-12-10 00:22:59 +0900

    :art:style : 레이아웃 꾸미기

--- front-server/src/App.js
+++ front-server/src/App.js
@@ -2,10 +2,12 @@
 import React from 'react'
 import SignUpPage from "./component/SignUpPage"
 import { BrowserRouter, Route , Routes } from 'react-router-dom';
+import Header from "./component/Header";
 
 function App() {
     return (
         <div className='App'>
+            <Header/>
             <BrowserRouter>
                 <Routes>
                     <Route path="/signUp" element={<SignUpPage />} />
