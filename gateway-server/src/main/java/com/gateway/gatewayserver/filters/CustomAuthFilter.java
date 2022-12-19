ebe5b37461388e70ab73f66932b6193fc5222fce
sienna <20200803@sungshin.ac.kr>
2022-12-19 19:23:41 +0900

    :recycle:refactor : Gateway 에러 처리 및 매직 넘버 상수화

--- gateway-server/src/main/java/com/gateway/gatewayserver/filters/CustomAuthFilter.java
+++ gateway-server/src/main/java/com/gateway/gatewayserver/filters/CustomAuthFilter.java
@@ -1,5 +1,6 @@
 package com.gateway.gatewayserver.filters;
 
+import com.gateway.gatewayserver.exception.NotFoundTokenException;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 
@@ -8,6 +9,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CustomAuthFilter extends AbstractGatewayFilterFactory<CustomAuthFilter.Config> {
+
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String TYPE_OF_AUTHORIZATION = "Bearer";
+    private static final String REPLACE_AUTHORIZATION = "";
+    private static final int TOKEN = 0;
+
     public CustomAuthFilter() {
         super(Config.class);
     }
@@ -17,11 +24,11 @@ public class CustomAuthFilter extends AbstractGatewayFilterFactory<CustomAuthFil
         return ((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             try {
-                String authorization = request.getHeaders().get("Authorization").get(0);
-                authorization.replace("Bearer", "");
+                String authorization = request.getHeaders().get(AUTHORIZATION).get(TOKEN);
+                authorization.replace(TYPE_OF_AUTHORIZATION, REPLACE_AUTHORIZATION);
                 return chain.filter(exchange);
             } catch (NullPointerException exception) {
-                throw new Error("token 이 없습니다");
+                throw new NotFoundTokenException();
             }
         });
     }
